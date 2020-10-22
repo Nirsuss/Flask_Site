1b60c435d08c0b332c5726fdb8e6d8b9ffc69e9b
Nirsuss <47823839+Nirsuss@users.noreply.github.com>
2020-10-22 19:40:44 +0500

    upgrate

--- hello_Flask_html — копия.py
+++ hello_Flask_html — копия.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, escape, session,copy_current_request_context #Импортирование класса Flask из модуля flask
+from flask import Flask, render_template, request, escape, session #Импортирование класса Flask из модуля flask
 from DBcm import UseDatabase, ConnectionError, CredentialError
 from search import search4letters
 from checker import check_logged_in
@@ -18,23 +18,22 @@ def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out'
 
-
+def log_request(req: 'flask_request', res: str) -> None:
+    """Журналирует веб-запрос и возвращает результаты"""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                     (phrase, letters, ip, browser_string, results)
+                      values
+                      (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res,))
 
 @app.route('/123', methods=['POST'])
 def do_search() ->'html':
 
-    @copy_current_request_context
-    def log_request(req: 'flask_request', res: str) -> None:
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """insert into log
-                                (phrase, letters, ip, browser_string, results)
-                                 values
-                                 (%s, %s, %s, %s, %s)"""
-            cursor.execute(_SQL, (req.form['phrase'],
-                                  req.form['letters'],
-                                  req.remote_addr,
-                                  req.user_agent.browser,
-                                  res,))
     phrase = request.form['phrase']
     letters =  request.form['letters']
     title = 'Твои результаты: '
