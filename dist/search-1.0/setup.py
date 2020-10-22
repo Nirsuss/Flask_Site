9e8a5e5c890cd347762f1601ea346168baa904e0
Nirsuss <47823839+Nirsuss@users.noreply.github.com>
2020-10-23 02:46:43 +0500

    pythonanywhere

---
+++ dist/search-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='search',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='Timur Karimov',
+    author_email='tumur.karimov@list.ru',
+    url='headfirstlab.com',
+    py_modules=['search'],
+)
---
+++ webapp/hello_Flask_html.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, escape, session,copy_current_request_context #Импортирование класса Flask из модуля flask
+from DBcm import UseDatabase, ConnectionError, CredentialError
+from search import search4letters
+from checker import check_logged_in
+from threading import Thread
+app = Flask(__name__)  #Создание объекта и присвание его переменной app
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearh',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }#Словарь с параметрами соедининения
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
+
+@app.route('/123', methods=['POST'])
+def do_search() ->'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                                (phrase, letters, ip, browser_string, results)
+                                 values
+                                 (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res,))
+    phrase = request.form['phrase']
+    letters =  request.form['letters']
+    title = 'Твои результаты: '
+    results = str(search4letters(phrase, letters))
+    try:
+       t = Thread(target=log_request, args=(request, results))
+       t.start()
+    except Exception as err:
+        print('*****Login failed with this error:', str(err)) #Универсальный обработчик исключений
+    return render_template('Result.html', the_phrase=phrase, the_letters=letters, the_results=results, the_title=title,)
+
+@app.route('/')#Декоратор с URL
+
+@app.route('/entry')
+@check_logged_in
+def entry_pages() ->'html':
+    return render_template('entry.html', The_Title='Welcome to search4letters on the web! ')
+
+@app.route('/viewlog')#Просмотр лог файла в браузере
+@check_logged_in
+def view_the_log() ->'html':
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results
+                         from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                                   The_Title='View Log',
+                                   the_row_titles=titles,
+                                   the_data=contents, )
+    except ConnectionError as err:
+        print('Is your DataBase switched on? Error:', str(err))
+    except CredentialError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
+app.secret_key = 'YouGenius'
+logged_in = False
+if __name__ == '__main__':#Заготовка под pythonanywhere
+    app.run(debug=True)
